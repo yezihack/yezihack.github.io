e0474b04b33be3d1d22ccd087004dad1e4275a7e
纯钧 <zgwszj@chunjun.com>
2019-08-07 18:06:32 +0800

    fix:delete theme

--- start.sh
+++ start.sh
@@ -23,11 +23,29 @@ function new_pages() {
     pages=${year}/${month}/${page_name}.md
     file=${dir}/content/$pages
     if [ -f $file ];then
-        echo $file" 文件已存在,请选择其它名称!"
+        echo "$file 文件已存在,请选择其它名称!"
         exit 0
     fi
     hugo new $pages
     echo "创建成功"
+}
+# 创建三省
+function new_bat() {
+    bat=${dir}/content/bluebat
+    if [ ! -d $bat ];then
+        mkdir -p $bat
+    fi
+    count=$((`cd ${bat};ls |wc -l`))
+    count=$(($count+1))
+    pages=bluebat/$(($count)).md
+    file=${bat}/$(($count)).md
+    if [ -f $file ];then
+        echo "$file 文件已存在,请选择其它名称!"
+        exit 0
+    fi
+    hugo new $pages
+    echo "创建成功"
+    sed -i "" "s/default.jpg/bluebat\/$count.jpg/g" $file
 }
 # 删除页面
 function rm_page() {
@@ -72,8 +90,14 @@ function issue() {
 
 # 选择
 case $cate in
+"dev")
+    hugo serve
+    ;;
 "new")
     new_pages $name
+    ;;
+"bat")
+    new_bat
     ;;
 "rm")
     rm_page $name
@@ -81,8 +105,9 @@ case $cate in
 "issue")
     issue $name
     ;;
-
-"help"|"h"|"?")
+"help"|"h"|"?"|*)
+    echo "请输入以下命令"
+    echo "start new bat 蝙蝠三省"
     echo "start new [页面名称,无须后缀名] ------创建文章页面"
     echo "start rm [页面名称,无须后缀名] ------删除文章页面"
     echo "start issue [可选,git提交备注] ------发布站点"
